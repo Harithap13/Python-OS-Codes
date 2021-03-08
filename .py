5fad1042b91ca72275ce200c409cbe3113ced927
Harithap13 <61133251+Harithap13@users.noreply.github.com>
2021-03-08 12:52:58 +0300

    Update and rename untitled0.py to .py

--- .py
+++ .py
@@ -4,8 +4,9 @@ Created on Thu Mar  4 10:35:39 2021
 
 @author: harit
 """
+# Code to list all the files in a directory without the extensions.
 import os
-entries = os.listdir('C:/Users/harit/Desktop/series 3')
+entries = os.listdir('C:/Users/path to directory')
 entries.sort()
 for entry in entries:
     ent=entry[:-4]
